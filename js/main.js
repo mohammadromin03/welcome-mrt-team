f2792dc4b51b3d229effd6e45655599a37f8bb53
mohammadromin03 <63338660+mohammadromin03@users.noreply.github.com>
2020-04-23 22:33:10 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function ($) {
+ "use strict";
+ $(function(){
+			
+				setTimeout(function(){
+					$('.fly-in-text').removeClass('mrt');
+				}, 500);
+			
+			});
+		
+		
+ 
+				/* setTimeout(function(){
+					$('.fly-in-text').removeClass('mrt');
+				}, 500); */
+ 
+})(jQuery); 
